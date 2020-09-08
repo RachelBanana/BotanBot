6c6a15f050c3c1ca112f828333d48aa4db73d265
RachelBanana <RachelBananaCreampie@gmail.com>
2020-09-09 01:33:11 +0800

    update botan's bday to 2021

--- app.py
+++ app.py
@@ -347,9 +347,7 @@ async def debut(res, msg):
     await res.channel.send(m)
 
 async def birthday(res, msg):
-    await res.channel.send("Happy Birthday Botan-sama~!!!")
-    return
-    bday = dtime(2020, 9, 8, tzinfo = timezone.utc)
+    bday = dtime(2021, 9, 8, tzinfo = timezone.utc)
     days, hours, minutes = time_until(bday)
     m = "Botan-sama's birthday is on 8th of September, just {} more day{} to go!".format(days, "s" * (days>1))
     await res.channel.send(m)
