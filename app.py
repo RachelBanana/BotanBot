f85838aadba24b43a9926139237400843d621650
RachelBanana <RachelBananaCreampie@gmail.com>
2021-01-17 18:00:11 +0800

    limit on_message_delete to only track botan guild

--- app.py
+++ app.py
@@ -1965,6 +1965,11 @@ async def on_member_update(before, after):
 # On message deleted
 @client.event
 async def on_message_delete(message):
+    ## if message from other servers, return
+    botan_guild = client.get_guild(d["discord_ids"]["guild"])
+    if message.guild or message.guild.id != botan_guild:
+        return
+
     ## if message is from bot, return
     if message.author == client.user:
         return
