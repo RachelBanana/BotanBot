92958456472ef2618647df34755078b1497b11e3
RachelBanana <RachelBananaCreampie@gmail.com>
2021-02-02 09:26:18 +0800

    re-add valentines command 2

--- app.py
+++ app.py
@@ -1811,7 +1811,7 @@ commands = {
 
 dm_commands = {
     "verify": verify_membership,
-    # "valentines": send_valentines_message,
+    "valentines": send_valentines_message,
     # horny exclusive commands
     "nsfw": nsfw_art,
     "add_nsfw": add_nsfw_art,
