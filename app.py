16cb3215761066a90ec43369a68c8afae4e00ec7
RachelBanana <RachelBananaCreampie@gmail.com>
2021-02-02 09:06:50 +0800

    new valentines dm command 2

--- app.py
+++ app.py
@@ -894,7 +894,7 @@ async def send_valentines_message(res, msg):
     if not msg:
         # show secret match name and command details
         m = "Your Valentine's secret match is {}, use the same command again along with a message to send them an anonymous letter!"
-        m += "\n``valentines {Message Here}``"
+        m += "\n``valentines {{Message}}``"
         await res.channel.send(m.format(str(target)))
         return
 
