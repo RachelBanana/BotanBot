0de84010775abfc296ca8085f53133cc1d0e54b2
RachelBanana <RachelBananaCreampie@gmail.com>
2021-02-10 11:12:59 +0800

    remove glitch

--- app.py
+++ app.py
@@ -2146,15 +2146,6 @@ async def on_message(res):
             "image": image_url
         })
 
-    if res.channel.id == d["discord_ids"]["secret_guardian_chat"] and not res.content.startswith(prefix):
-        # randomly 1 in 5 chances
-        if random.randint(1, 5) == 3 or "aloe" in res.content:
-            chars = "aeolmnAEOLMN"
-            m = "".join("-" if c in chars else c for c in res.content)
-
-            await res.delete()
-            await res.channel.send(m)
-
     # !!! valentines
 
     # checks if message needs attention (has prefix)
