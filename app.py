778f087c83228fc366bf24a85102f631b52b07ce
RachelBanana <RachelBananaCreampie@gmail.com>
2021-02-22 14:09:18 +0800

    remove restrain

--- app.py
+++ app.py
@@ -2253,10 +2253,6 @@ async def on_message(res):
     action = commands.get(cmd, None)
     if action:
         await action(res, msg)
-    
-    # !!! 
-    if str(res.author) != owner:
-        return
 
     # check for admin/mod permission and if admin command exists, perform action
     if not (res.author.guild_permissions.administrator or any(role.id == d["discord_ids"]["mod_role"] for role in res.author.roles)):
